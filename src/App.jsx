6c7fa727367da3a24030777151dc9afcac19ab8e
martaalcazarr <martaalcazarredondo@gmail.com>
2023-08-23 11:14:14 -0400

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-
-
+import { Container, Grid, Typography } from "@mui/material"
+import Form from "./components/Form"
 function App() {
   
 
   return (
-    <h1>news</h1>
+    <Container>
+      <header>
+        <Typography align="center" marginY={5} component="h1" variant="h3">
+          Find the latest news
+        </Typography>
+      </header>
+
+      <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center">
+        <Grid item xs={12} md={6} lg={4}>
+          <Form />
+        </Grid>
+      </Grid>
+      
+    </Container>
   )
 }
 
